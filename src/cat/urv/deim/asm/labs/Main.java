86023ba7e42b01f61f6caf3fb1bb5eb5728afde2
M4rkVs <marc.sabatema@estudiants.urv.cat>
2021-03-17 17:36:06 +0100

    Codi classe main copiat

---
+++ src/cat/urv/deim/asm/labs/Main.java
@@ -0,0 +1,26 @@
+package cat.urv.deim.asm.labs;
+/*
+import cat.urv.deim.asm.labs.core.Dalvik;
+import cat.urv.deim.asm.labs.factory.ActivityFactory;
+import cat.urv.deim.asm.labs.interfaces.IActivity;
+*/
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        System.out.println("Execució primera activity");
+        System.out.println("========================");
+/*        IActivity mainActivity = ActivityFactory.build(MainActivity.class.getName());
+        Dalvik.execute(mainActivity);
+        System.out.println("\n");
+        System.out.println("Execució segona activity");
+        System.out.println("========================");
+        IActivity mainActivity2 = ActivityFactory.build(MainActivity.class);
+        Dalvik.execute(mainActivity2);
+        Dalvik.stop(mainActivity2);
+        Dalvik.free(mainActivity2);
+ */
+    }
+}
